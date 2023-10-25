bd2143e73b7f5ff493b7120578fef33777845724
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-25 14:12:13 +0700

    intercepting routes

---
+++ src/app/product/@modal/(.)detail/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { getData } from "@/app/services/products/index";
+import Modal from "@/components/core/Modal/index";
+
+export default async function DetailProductPage(props: any) {
+  const { params } = props;
+  const product = await getData(
+    "http://localhost:3000/api/product/?id=" + params.id
+  );
+
+  return (
+    <Modal>
+      <img
+        src={product.data.image}
+        alt=""
+        className="w-full object-cover aspect-square col-span-2"
+      />
+      <div className="bg-white py-4 px-6">
+        <h3>{product.data.title}</h3>
+        <p>Price: ${product.data.price}</p>
+      </div>
+    </Modal>
+  );
+}
